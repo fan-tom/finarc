6abb5122403114d3bf58893cd08020051df9380a
Alexey Gerasimov <lokomot476@gmail.com>
2020-04-04 15:44:14 +0500

    Add ?Sized bound to T Implement PartialEq, Eq, Ord, Debug, Display traits

--- src/lib.rs
+++ src/lib.rs
@@ -19,14 +19,17 @@
 //! Unlike `Arc`, `FinArc<T, F>` implements `DerefMut` to `T`, because each instance of `FinArc` owns
 //! its own copy of `T`
 
-use std::ops::{Deref, DerefMut};
-use std::sync::Arc;
-use std::ptr;
+use std::cmp::Ordering;
+use std::fmt;
 use std::mem;
+use std::ops::{Deref, DerefMut};
+use std::ptr;
+use std::sync::Arc;
 
 pub struct FinArc<T, F>
-    where
-        F: FnOnce(&mut T),
+where
+    T: ?Sized,
+    F: FnOnce(&mut T),
 {
     // We will use this field to both don't clone finalizer and to detect when last instance is dropped
     // Option here as FnOnce accepts `self` by value, and we can take Arc to try to get finalizer if it is possible
@@ -36,8 +39,16 @@ pub struct FinArc<T, F>
 }
 
 impl<T, F> FinArc<T, F>
-    where F: FnOnce(&mut T)
+where
+    F: FnOnce(&mut T),
 {
+    pub fn new(data: T, finalizer: F) -> Self {
+        Self {
+            inner: Some(Arc::new(finalizer)),
+            data,
+        }
+    }
+
     /// Returns the contained value, if this is the last instance of FinArc, without running finalizer
     ///
     /// Otherwise, an [`Err`][result] is returned with the same `FinArc` that was
@@ -75,21 +86,10 @@ impl<T, F> FinArc<T, F>
     }
 }
 
-impl<T, F> FinArc<T, F>
-    where
-        F: FnOnce(&mut T),
-{
-    pub fn new(data: T, finalizer: F) -> Self {
-        Self {
-            inner: Some(Arc::new(finalizer)),
-            data,
-        }
-    }
-}
-
 impl<T, F> Deref for FinArc<T, F>
-    where
-        F: FnOnce(&mut T),
+where
+    T: ?Sized,
+    F: FnOnce(&mut T),
 {
     type Target = T;
 
@@ -99,8 +99,9 @@ impl<T, F> Deref for FinArc<T, F>
 }
 
 impl<T, F> DerefMut for FinArc<T, F>
-    where
-        F: FnOnce(&mut T),
+where
+    T: ?Sized,
+    F: FnOnce(&mut T),
 {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.data
@@ -108,9 +109,9 @@ impl<T, F> DerefMut for FinArc<T, F>
 }
 
 impl<T, F> Clone for FinArc<T, F>
-    where
-        T: Clone,
-        F: FnOnce(&mut T),
+where
+    T: Clone,
+    F: FnOnce(&mut T),
 {
     fn clone(&self) -> Self {
         Self {
@@ -121,8 +122,9 @@ impl<T, F> Clone for FinArc<T, F>
 }
 
 impl<T, F> Drop for FinArc<T, F>
-    where
-        F: FnOnce(&mut T),
+where
+    T: ?Sized,
+    F: FnOnce(&mut T),
 {
     fn drop(&mut self) {
         // Here we both checked that it is the last instance and got callback from it, double win!
@@ -130,6 +132,186 @@ impl<T, F> Drop for FinArc<T, F>
         if let Ok(f) = Arc::try_unwrap(self.inner.take().expect("Finalizer is gone")) {
             (f)(&mut self.data);
         }
+    }
+}
+
+/// We ignore finalizers when comparing FinArc's, so they may be of different types
+impl<T, F, F1> PartialEq<FinArc<T, F1>> for FinArc<T, F>
+where
+    T: ?Sized + PartialEq,
+    F: FnOnce(&mut T),
+    F1: FnOnce(&mut T),
+{
+    /// Equality for two `FinArc`s.
+    ///
+    /// Two `FinArc`s are equal if their inner values are equal.
+    ///
+    /// If `T` also implements `Eq`, two `FinArc`s that point to the same value are
+    /// always equal.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use finarc::FinArc;
+    ///
+    /// let five = FinArc::new(5, |_|{});
+    ///
+    /// assert!(five == FinArc::new(5, |_|{}));
+    /// ```
+    #[inline]
+    fn eq(&self, other: &FinArc<T, F1>) -> bool {
+        (**self).eq(&**other)
+    }
+
+    /// Inequality for two `FinArc`s.
+    ///
+    /// Two `FinArc`s are unequal if their inner values are unequal.
+    ///
+    /// If `T` also implements `Eq`, two `FinArc`s that point to the same value are
+    /// never unequal.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use finarc::FinArc;
+    ///
+    /// let five = FinArc::new(5, |_|{});
+    ///
+    /// assert!(five != FinArc::new(6, |_|{}));
+    /// ```
+    #[inline]
+    fn ne(&self, other: &FinArc<T, F1>) -> bool {
+        (**self).ne(&**other)
+    }
+}
+
+/// We ignore finalizers when comparing FinArc's, so they may be of different types
+impl<T, F, F1> PartialOrd<FinArc<T, F1>> for FinArc<T, F>
+where
+    T: ?Sized + PartialOrd,
+    F: FnOnce(&mut T),
+    F1: FnOnce(&mut T),
+{
+    /// Partial comparison for two `FinArc`s.
+    ///
+    /// The two are compared by calling `partial_cmp()` on their inner values.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use finarc::FinArc;
+    /// use std::cmp::Ordering;
+    ///
+    /// let five = FinArc::new(5, |_|{});
+    ///
+    /// assert_eq!(Some(Ordering::Less), five.partial_cmp(&FinArc::new(6, |_|{})));
+    /// ```
+    fn partial_cmp(&self, other: &FinArc<T, F1>) -> Option<Ordering> {
+        (**self).partial_cmp(&**other)
+    }
+
+    /// Less-than comparison for two `FinArc`s.
+    ///
+    /// The two are compared by calling `<` on their inner values.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use finarc::FinArc;
+    ///
+    /// let five = FinArc::new(5, |_|{});
+    ///
+    /// assert!(five < FinArc::new(6, |_|{}));
+    /// ```
+    fn lt(&self, other: &FinArc<T, F1>) -> bool {
+        *(*self) < *(*other)
+    }
+
+    /// 'Less than or equal to' comparison for two `FinArc`s.
+    ///
+    /// The two are compared by calling `<=` on their inner values.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use finarc::FinArc;
+    ///
+    /// let five = FinArc::new(5, |_|{});
+    ///
+    /// assert!(five <= FinArc::new(5, |_|{}));
+    /// ```
+    fn le(&self, other: &FinArc<T, F1>) -> bool {
+        *(*self) <= *(*other)
+    }
+
+    /// Greater-than comparison for two `FinArc`s.
+    ///
+    /// The two are compared by calling `>` on their inner values.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use finarc::FinArc;
+    ///
+    /// let five = FinArc::new(5, |_|{});
+    ///
+    /// assert!(five > FinArc::new(4, |_|{}));
+    /// ```
+    fn gt(&self, other: &FinArc<T, F1>) -> bool {
+        *(*self) > *(*other)
+    }
+
+    /// 'Greater than or equal to' comparison for two `FinArc`s.
+    ///
+    /// The two are compared by calling `>=` on their inner values.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use finarc::FinArc;
+    ///
+    /// let five = FinArc::new(5, |_|{});
+    ///
+    /// assert!(five >= FinArc::new(5, |_|{}));
+    /// ```
+    fn ge(&self, other: &FinArc<T, F1>) -> bool {
+        *(*self) >= *(*other)
+    }
+}
+
+impl<T: ?Sized + Ord, F: FnOnce(&mut T)> Ord for FinArc<T, F> {
+    /// Comparison for two `FinArc`s.
+    ///
+    /// The two are compared by calling `cmp()` on their inner values.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use finarc::FinArc;
+    /// use std::cmp::Ordering;
+    ///
+    /// let five = FinArc::new(5, |_|{});
+    /// let mut six = FinArc::clone(&five);
+    /// *six = 6;
+    ///
+    /// assert_eq!(Ordering::Less, five.cmp(&six));
+    /// ```
+    fn cmp(&self, other: &FinArc<T, F>) -> Ordering {
+        (**self).cmp(&**other)
+    }
+}
+
+impl<T: ?Sized + Eq, F: FnOnce(&mut T)> Eq for FinArc<T, F> {}
+
+impl<T: ?Sized + fmt::Display, F: FnOnce(&mut T)> fmt::Display for FinArc<T, F> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(&**self, f)
+    }
+}
+
+impl<T: ?Sized + fmt::Debug, F: FnOnce(&mut T)> fmt::Debug for FinArc<T, F> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(&**self, f)
     }
 }
 
