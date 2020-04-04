707487b943ce4d65b249e20a5b73be871eac493c
Alexey Gerasimov <lokomot476@gmail.com>
2020-04-04 14:23:35 +0500

    Doc and formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,32 @@
+//! This crate provides type FinArc, which is Arc with finalizer callback, that
+//! clones inner data on cloning and calls finalizer when last instance is dropped
+//!
+//! It may be useful in situations where you have internally synchronized type that is `Clone`able,
+//! but all its clones belongs to single resource and that resource must be released when last clone is dropped
+//!
+//! One such example is `Channel` in `lapin` crate, that belongs to RabbitMQ channel.
+//! It is cloneable, but it doesn't follow RAII, that is, no close on the drop
+//! and any thread that uses one copy of channel may close it, leaving other threads to face
+//! Rabbit errors when they try to use this already closed channel.
+//!
+//! Suppose you want to create your own wrapper around this type.
+//! You may use `Arc<Mutex/RwLock<Channel>>`, but it is inefficient, as Channel is already synchronized
+//! (it is simply bunch of `Arc<Mutex/RwLock>`s), so you need to clone them, but in the same time, track
+//! the number of copies to call `.close()` on last instance before drop.
+//! FinArc allows you to do that, you just provide `FnOnce(&mut Channel)` callback, that is called
+//! when last channel copy is dropped.
+//!
+//! Unlike `Arc`, `FinArc<T, F>` implements `DerefMut` to `T`, because each instance of `FinArc` owns
+//! its own copy of `T`
+
 use std::ops::{Deref, DerefMut};
 use std::sync::Arc;
 use std::ptr;
 use std::mem;
 
-/// This crate provides type FinArc, which is Arc with finalizer callback, that
-/// clones inner data on cloning and calls finalizer when last instance is dropped
-///
-/// It may be useful in situations where you have internally synchronized type that is `Clone`able,
-/// but all its clones belongs to single resource and that resource must be freed when last clone is dropped
-///
-/// One such example is `Channel` in `lapin` crate, that belongs to RabbitMQ channel.
-/// It is cloneable, but it doesn't follow RAII, so, no close on the drop
-/// and any thread that uses one copy of channel may close it, leaving other threads to face
-/// rabbit errors when they try to use this already closed channel.
-///
-/// Suppose you want to create your own wrapper around this rather unsafe type.
-/// You may use `Arc<Mutex/RwLock<Channel>>`, but it is inefficient, as Channel is already synchronized
-/// (it is simply bunch of `Arc<Mutex/RwLock>`s), so you need to clone them, but in the same time, track
-/// the number of copies to call `.close()` on last instance before drop.
-/// FinArc allows you to do that, you just provides `FnOnce(&mut Channel)` callback, that is called
-/// when last channel copy is dropped.
-///
-/// Unlike `Arc`, `FinArc<T, F>` implements `DerefMut` to `T`, because each instance of `FinArc` owns
-/// its own copy of `T`
 pub struct FinArc<T, F>
-where
-    F: FnOnce(&mut T),
+    where
+        F: FnOnce(&mut T),
 {
     // We will use this field to both don't clone finalizer and to detect when last instance is dropped
     // Option here as FnOnce accepts `self` by value, and we can take Arc to try to get finalizer if it is possible
@@ -75,8 +76,8 @@ impl<T, F> FinArc<T, F>
 }
 
 impl<T, F> FinArc<T, F>
-where
-    F: FnOnce(&mut T),
+    where
+        F: FnOnce(&mut T),
 {
     pub fn new(data: T, finalizer: F) -> Self {
         Self {
@@ -87,8 +88,8 @@ where
 }
 
 impl<T, F> Deref for FinArc<T, F>
-where
-    F: FnOnce(&mut T),
+    where
+        F: FnOnce(&mut T),
 {
     type Target = T;
 
@@ -98,8 +99,8 @@ where
 }
 
 impl<T, F> DerefMut for FinArc<T, F>
-where
-    F: FnOnce(&mut T),
+    where
+        F: FnOnce(&mut T),
 {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.data
@@ -107,9 +108,9 @@ where
 }
 
 impl<T, F> Clone for FinArc<T, F>
-where
-    T: Clone,
-    F: FnOnce(&mut T),
+    where
+        T: Clone,
+        F: FnOnce(&mut T),
 {
     fn clone(&self) -> Self {
         Self {
@@ -120,8 +121,8 @@ where
 }
 
 impl<T, F> Drop for FinArc<T, F>
-where
-    F: FnOnce(&mut T),
+    where
+        F: FnOnce(&mut T),
 {
     fn drop(&mut self) {
         // Here we both checked that it is the last instance and got callback from it, double win!
